1edca6e2d44d873e6590a4fc22e91644aa7d8394
arpan-ti <arpan.gupta@trilogy.com>
2022-07-04 19:05:42 +0530

    testing devflows gchat

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
 def get_personal_message() -> str:
     # Modify the personal message as desired.
     name: str = "Arpan Gupta"
-    return f"Hello, my name i  {name}."
+    return f"Hello, my name is  {name}."
